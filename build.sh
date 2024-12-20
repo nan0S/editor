a71fe8eac100b5a83137a8bef32bfc9595eb3ad7
nan0S <hubert.obrzut@gmail.com>
2024-12-20 01:37:14 +0000

    improve build script

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 
-mkdir -p build
-pushd build
+build="$(dirname "$0")/build"
+mkdir -p "$build"
+save_dir=$(pwd) # /bin/sh doesn't have pushd/popd and I don't want to use /bin/bash
+cd "$build"
 
 if ! [ -e "build.exe" ]; then gcc -O0 ../code/build.cpp -o build.exe; fi
 ./build.exe "$@"
 if [ -e "build_tmp.exe" ]; then mv build_tmp.exe build.exe; fi
 
-popd
+cd "$save_dir"
